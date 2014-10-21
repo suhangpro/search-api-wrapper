da7385a2ef3605af3e993258e17402783da9679f
hangsu <suhangpro@gmail.com>
2014-10-21 11:19:14 -0400

    unicode support

--- bing/bing_search_api.py
+++ bing/bing_search_api.py
@@ -5,7 +5,7 @@ import string
 import argparse
 
 class BingSearchAPI():
-    bing_api = "https://api.datamarket.azure.com/Bing/Search/v1/Composite?"
+    bing_api = u"https://api.datamarket.azure.com/Bing/Search/v1/Composite?"
     
     def __init__(self, key):
         self.key = key
@@ -14,27 +14,27 @@ class BingSearchAPI():
         # Custom urlencoder.
         # They specifically want %27 as the quotation which is a single quote '
         # We're going to map both ' and " to %27 to make it more python-esque
-        request = string.replace(request, "'", '%27')
-        request = string.replace(request, '"', '%27')
-        request = string.replace(request, '+', '%2b')
-        request = string.replace(request, ' ', '%20')
-        request = string.replace(request, ':', '%3a')
+        request = string.replace(request, u"'", u'%27')
+        request = string.replace(request, u'"', u'%27')
+        request = string.replace(request, u'+', u'%2b')
+        request = string.replace(request, u' ', u'%20')
+        request = string.replace(request, u':', u'%3a')
         return request
         
     def search(self, query, start=1,total=10,searchImage=False):
         if searchImage:
-            request_prefix = 'Sources="image"'
+            request_prefix = u'Sources="image"'
         else:
-            request_prefix = 'Sources="web"'
-        request_prefix += '&Query="'  + str(query) + '"'
-        request_prefix += '&$format=json'
+            request_prefix = u'Sources="web"'
+        request_prefix += u'&Query="'  + query + u'"'
+        request_prefix += u'&$format=json'
 
         cnt = 0;
         skip = start - 1
         resList = []
         while cnt < total:
             num = min(50,total-cnt)
-            request = request_prefix + '&$top=' + str(num) + '&$skip=' + str(skip)
+            request = request_prefix + u'&$top=' + unicode(num) + u'&$skip=' + unicode(skip)
             request = self.replace_symbols(request)
             res = requests.get(self.bing_api+request, auth=(self.key, self.key)).json()
             skip = skip + num
@@ -55,7 +55,7 @@ def main():
     parser.add_argument('-i','--image',help="search images [False]",action='store_true',default=0)
     parser.add_argument('-k','--key-file',help="file storing API key [bingapi.key]",default='bingapi.key');
     args = parser.parse_args()
-    
+
     # 1050 results at most
     if args.n >1050:
         raise ValueError("Bing Search API returns at most 1050 results for each query")
@@ -67,11 +67,11 @@ def main():
 
     # api call
     bing = BingSearchAPI(apiKey)
-    results = bing.search(query=args.query,total=args.n,searchImage=args.image)
+    results = bing.search(query=unicode(args.query,'utf-8'),total=args.n,searchImage=args.image)
     
     # display results
     for url in results: 
-        print(url)
+        print(url.encode('utf-8'))
 
 if __name__ == "__main__":
     main()
